725c2218050fd4367167c93cbf8a60042fbbce82
Melissa <105521312+k0106@users.noreply.github.com>
2023-03-14 09:53:09 -0500

    Agregando funciones al Dom

--- 02-NombreVariables.js
+++
@@ -1,4 +0,0 @@
-const nombreEmpleado="Melissa Gordon";
-let direccion= "Cartagena";
-var costoDelPasaje = 5000;
-let tasaEmbarque =10000;
---
+++ HolaMundo.js
@@ -0,0 +1,14 @@
+const btn=document.querySelector('[data form-btn]');
+
+console.log(btn);
+
+
+btn.addEventListener('click',function () {
+    console.log('crear tarea');
+    const input=document.querySelector('[ data-form-input]');
+    console.log(input.value);
+
+
+
+})
+
